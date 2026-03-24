0af44ec6d77c2e611313dbe64f723164ef2f2897
deepakb-cd <deepakb@clouddestinations.com>
2026-03-24 11:27:35 +0530

    test

--- AWS_infra/ec2.tf
+++ AWS_infra/ec2.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "splunk_ec2" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
-  vpc_security_group_ids = var.security_group_id
+  vpc_security_group_ids = [var.security_group_id]
   key_name               = var.key_name
 
   tags = {
