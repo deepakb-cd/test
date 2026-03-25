4651bf46e67100a9853dcdf5798bb1bd77ae75ed
deepakb-cd <deepakb@clouddestinations.com>
2026-03-25 16:21:56 +0530

    Cloudtrial

--- AWS_infra/modules/cloudtrail/variable.tf
+++ AWS_infra/modules/cloudtrail/variable.tf
@@ -1,21 +1,21 @@
-# variable "name" {
-#   type = string
-# }
+variable "name" {
+  type = string
+}
 
-# variable "bucket_name" {
-#   type = string
-# }
+variable "bucket_name" {
+  type = string
+}
 
-# variable "kms_key_arn" {
-#   type    = string
-#   default = ""
-# }
+variable "kms_key_arn" {
+  type    = string
+  default = ""
+}
 
-# variable "s3_key_prefix" {
-#   type    = string
-#   default = ""
-# }
+variable "s3_key_prefix" {
+  type    = string
+  default = ""
+}
 
-# variable "tags" {
-#   type = map(string)
-# }
+variable "tags" {
+  type = map(string)
+}
