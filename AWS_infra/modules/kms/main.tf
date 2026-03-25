f34bf4aedd6e90f0b73bf3a64c87ae3532488016
deepakb-cd <deepakb@clouddestinations.com>
2026-03-25 13:55:16 +0530

    KMS deletion

--- AWS_infra/modules/kms/main.tf
+++ AWS_infra/modules/kms/main.tf
@@ -1,54 +1,54 @@
-# data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {}
 
-# locals {
-#   base_policy = [
-#     {
-#       Sid = "EnableRootPermissions"
-#       Effect = "Allow"
-#       Principal = {
-#         AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-#       }
-#       Action   = "kms:*"
-#       Resource = "*"
-#     }
-#   ]
+locals {
+  base_policy = [
+    {
+      Sid = "EnableRootPermissions"
+      Effect = "Allow"
+      Principal = {
+        AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+      }
+      Action   = "kms:*"
+      Resource = "*"
+    }
+  ]
 
-#   cloudtrail_policy = var.enable_cloudtrail ? [
-#   {
-#     Sid = "AllowCloudTrailUse"
-#     Effect = "Allow"
-#     Principal = {
-#       Service = "cloudtrail.amazonaws.com"
-#     }
-#     Action = [
-#       "kms:GenerateDataKey*",
-#       "kms:Encrypt",
-#       "kms:Decrypt",
-#       "kms:ReEncrypt*",
-#       "kms:DescribeKey"
-#     ]
-#     Resource = "*"
-#   }
-# ] : []
-# }
+  cloudtrail_policy = var.enable_cloudtrail ? [
+  {
+    Sid = "AllowCloudTrailUse"
+    Effect = "Allow"
+    Principal = {
+      Service = "cloudtrail.amazonaws.com"
+    }
+    Action = [
+      "kms:GenerateDataKey*",
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:DescribeKey"
+    ]
+    Resource = "*"
+  }
+] : []
+}
 
-# resource "aws_kms_key" "this" {
-#   description         = var.description
-#   enable_key_rotation = true
+resource "aws_kms_key" "this" {
+  description         = var.description
+  enable_key_rotation = true
 
-#   policy = jsonencode({
-#     Version   = "2012-10-17"
-#     Statement = concat(local.base_policy, local.cloudtrail_policy)
-#   })
+  policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = concat(local.base_policy, local.cloudtrail_policy)
+  })
 
-#   tags = var.tags
-# }
+  tags = var.tags
+}
 
-# resource "aws_kms_alias" "this" {
-#   name          = var.alias
-#   target_key_id = aws_kms_key.this.id
-# }
+resource "aws_kms_alias" "this" {
+  name          = var.alias
+  target_key_id = aws_kms_key.this.id
+}
 
-# output "arn" {
-#   value = aws_kms_key.this.arn
-# }
+output "arn" {
+  value = aws_kms_key.this.arn
+}
