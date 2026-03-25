7c0378dcc29e2aee7eb10584ceabc22d49da80c1
deepakb-cd <deepakb@clouddestinations.com>
2026-03-25 13:52:29 +0530

    KMS deletion

--- AWS_infra/cloudtrial.tf
+++ AWS_infra/cloudtrial.tf
@@ -1,18 +1,18 @@
-module "cloudtrail" {
-  source   = "./modules/cloudtrail"
-  for_each = var.cloudtrails
+# module "cloudtrail" {
+#   source   = "./modules/cloudtrail"
+#   for_each = var.cloudtrails
 
-  name          = each.value.name
-  bucket_name   = module.s3_buckets[each.value.bucket_ref].bucket_id
-  kms_key_arn   = try(module.kms[each.value.kms_ref].arn, "")
-  s3_key_prefix = each.value.s3_key_prefix
-  tags          = each.value.tags
+#   name          = each.value.name
+#   bucket_name   = module.s3_buckets[each.value.bucket_ref].bucket_id
+#   kms_key_arn   = try(module.kms[each.value.kms_ref].arn, "")
+#   s3_key_prefix = each.value.s3_key_prefix
+#   tags          = each.value.tags
 
-  ########################################
-  # IMPORTANT: ensure S3 + KMS ready
-  ########################################
-  depends_on = [
-    module.s3_buckets,
-    module.kms
-  ]
-}
+#   ########################################
+#   # IMPORTANT: ensure S3 + KMS ready
+#   ########################################
+#   depends_on = [
+#     module.s3_buckets,
+#     module.kms
+#   ]
+# }
