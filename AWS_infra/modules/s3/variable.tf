549e4b5f09aebeafe099416339d6aff55d400c78
deepakb-cd <deepakb@clouddestinations.com>
2026-03-26 15:39:11 +0530

    S3

--- AWS_infra/modules/s3/variable.tf
+++ AWS_infra/modules/s3/variable.tf
@@ -1,22 +1,22 @@
-# variable "bucket_name" {}
-# variable "tags" { type = map(string) }
-# variable "versioning" {}
-# variable "encryption" {}
-# variable "block_public" {}
-# variable "object_keys" { type = list(string) }
+variable "bucket_name" {}
+variable "tags" { type = map(string) }
+variable "versioning" {}
+variable "encryption" {}
+variable "block_public" {}
+variable "object_keys" { type = list(string) }
 
-# variable "enable_cloudtrail_policy" {
-#   type    = bool
-#   default = false
-# }
+variable "enable_cloudtrail_policy" {
+  type    = bool
+  default = false
+}
 
-# # 🔥 IMPORTANT
-# variable "s3_key_prefix" {
-#   type    = string
-#   default = ""
-# }
 
-# variable "kms_key_arn" {
-#   type    = string
-#   default = ""
-# }
+variable "s3_key_prefix" {
+  type    = string
+  default = ""
+}
+
+variable "kms_key_arn" {
+  type    = string
+  default = ""
+}
